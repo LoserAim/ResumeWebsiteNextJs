f92124a1140fafe37d46aa8ba77550371ba587fb
Andrew Doser <Andrew.Doser@pkisolutions.com>
2025-03-30 16:46:10 -0700

    Enhancing Profile Card

---
+++ resume-website/src/_components/typography/ListIcon.tsx
@@ -0,0 +1,25 @@
+﻿import React from 'react';
+import {ListItemIcon, ListItemText} from "@mui/material";
+
+interface IListIconProps {
+    icon: React.ReactNode;
+    title: string;
+    subtitle: string;
+}
+
+const ListIcon = ({
+                      icon,
+                      title,
+                      subtitle
+                  }: IListIconProps) => {
+    return (
+        <>
+            <ListItemIcon>
+                {icon}
+            </ListItemIcon>
+            <ListItemText primary={title} secondary={subtitle}/>
+        </>
+    );
+};
+
+export default ListIcon;
---
+++ resume-website/src/_constants/ProfileData.tsx
@@ -0,0 +1,117 @@
+﻿import * as React from "react";
+import PhoneIcon from "@mui/icons-material/Phone";
+import EmailIcon from "@mui/icons-material/Email";
+
+type TIcon = {
+    icon: React.ReactNode;
+    title: string;
+    subtitle: string;
+}
+type TListIcon = TIcon[];
+export const profile: TListIcon = [
+    {
+        icon: <PhoneIcon/>,
+        title: "Phone",
+        subtitle: "+1 (503) 501-9827"
+    },
+    {
+        icon: <EmailIcon/>,
+        title: "Email",
+        subtitle: "drew.doser@gmail.com"
+    }
+]
+
+type TSkill = {
+    title: string;
+    subtitle: string[];
+}
+type TSkills = TSkill[]
+
+export const Skills: TSkills = [
+    {
+        title: "Programming Languages",
+        subtitle: [
+            "Typescript", "C#", "Python", "Dart", "C++", "Java"
+        ]
+    },
+    {
+        title: "Databases",
+        subtitle: [
+            "MSSQL", "MongoDB", "ELK Stack", "PostgreSQL", "CosmosDB"
+        ]
+    },
+    {
+        title: "Development Tools",
+        subtitle: [
+            "Rider", "Webstorm", "Pycharm", "Visual Studio", "VS Code#"
+        ]
+    },
+
+    {
+        title: "Web Technologies",
+        subtitle: [
+            "HTML",
+            "CSS",
+            "XPath",
+            "jQuery"
+        ]
+    },
+    {
+        title: "Cloud Platforms",
+        subtitle: [
+            "AWS",
+            "Azure",
+            "Atlas",
+        ]
+    },
+    {
+        title: "Version Control",
+        subtitle: [
+            "Github",
+            "Bitbucket",
+            "Gitlab",
+        ]
+    },
+    {
+        title: "Framework & Libraries",
+        subtitle: [
+            "React",
+            "Next.js",
+            "Express.js",
+            "Asp.Net Core",
+            "Entity Framework",
+            "Flask",
+            "Selenium",
+            "Jest",
+            "Xamarin",
+            "Flutter",
+        ]
+    },
+    {
+        title: "Project Management",
+        subtitle: [
+            "Jira",
+            "Monday",
+            "Github Projects",
+            "Slack",
+            "Teams",
+        ]
+    }
+];
+
+export const Expertise: string[] = [
+    "New Technologies Integration",
+    "Data Migration",
+    "Database Management",
+    "Wireframing & Design",
+    "Responsive UI/UX Design",
+    "Software Development Lifecycle",
+    "Testing & Debugging",
+    "Technical Leadership",
+    "Project Coordination",
+    "API Management",
+    "Performance Optimization",
+    "Cross-Functional Collaborations",
+    "Code Review",
+    "Technical Issues Resolution"
+]
