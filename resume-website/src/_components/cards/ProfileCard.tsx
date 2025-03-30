6c408a472a553e69ec9e7c023713e6ad23b0e60a
Andrew Doser <andrew.doser@pkisolutions.com>
2025-03-30 16:48:41 -0700

    Enhancing Profile Card (#3)
    Changes Made:
    
    - [x] Redid the entire Profile Card, `resume-website/src/_components/cards/ProfileCard.tsx`
    - [x] Added generic component for lists, `resume-website/src/_components/typography/ListIcon.tsx`
    - [x] Added a constants file for profile data, `resume-website/src/_constants/ProfileData.tsx`
    
    Need to plan out a possible database implementation to abstract personal data from the repository. For now, a constants file is fine for now.

--- resume-website/src/_components/cards/ProfileCard.tsx
+++ resume-website/src/_components/cards/ProfileCard.tsx
@@ -1,32 +1,69 @@
 ﻿import * as React from 'react';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-
 import Typography from '@mui/material/Typography';
+import {Avatar, Box, ListItemButton, ListItemText, Stack} from "@mui/material";
+import Divider from "@mui/material/Divider";
+import List from '@mui/material/List';
+import CardContent from "@mui/material/CardContent";
+import ListIcon from "@/_components/typography/ListIcon";
+import {Expertise, profile, Skills} from "@/_constants/ProfileData";
 
 export default function ProfileCard() {
     return (
-        <Card sx={{ maxWidth: "inherit" }}>
-            <CardMedia
-                sx={{ height: 400 }}
-                image="profile_pic.png"
-                title="andrew headshot"
-            />
+        <Card sx={{maxWidth: "inherit"}}>
+            <Box sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 2}}>
+                <Avatar alt={"andrew-headshot"} src={"profile_pic.png"} sx={{width: 100, height: 100}}/>
+                <Typography variant="h5" component="div" sx={{mt: 2}}>
+                    Andrew Doser
+                </Typography>
+            </Box>
+            <Divider sx={{mt: 2}}/>
             <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                    Andrew
-                </Typography>
-                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                    Andrews are a rare breed of pale and gangly but smart and intelligent.
-                </Typography>
+                <List component="nav" dense>
+                    {profile.map((item, index) => {
+                        return (
+                            <ListItemButton key={index}>
+                                <ListIcon icon={item.icon} title={item.title} subtitle={item.subtitle}/>
+                            </ListItemButton>
+                        );
+                    })}
+                </List>
             </CardContent>
-            <CardActions>
-                <Button size="small">Share</Button>
-                <Button size="small">Learn More</Button>
-            </CardActions>
+            <Divider/>
+            <CardContent>
+                <Stack>
+                    <Typography variant="h6" component="div">
+                        Areas of Expertise
+                    </Typography>
+                    <List component="nav" dense>
+                        {Expertise.map((item, index) => {
+                            return (
+                                <ListItemButton key={index}>
+                                    <ListItemText primary={item} />
+                                </ListItemButton>
+                            );
+                        })}
+                    </List>
+                </Stack>
+            </CardContent>
+            <Divider/>
+            <CardContent>
+                <Stack>
+                    <Typography variant="h6" component="div">
+                        Skills
+                    </Typography>
+                    <List component="nav" dense>
+                        {Skills.map((item, index) => {
+                            return (
+                                <ListItemButton key={index}>
+                                    <ListItemText primary={item.title}
+                                                  secondary={item.subtitle.join(", ")}/>
+                                </ListItemButton>
+                            );
+                        })}
+                    </List>
+                </Stack>
+            </CardContent>
         </Card>
     );
 }
