37d2e800c4d5f2207d78b9f03cad96a913e55beb
Andrew Doser <andrew.doser@pkisolutions.com>
2025-03-30 16:04:07 -0700

    Adding Education Card (#2)
    Changes Made:
    - [x] modified tagging in github yml file, `.github/workflows/build-docker-image.yml`
    - [x] Added a new card, `resume-website/src/_components/cards/EducationCard.tsx`
    - [x] Added a typography entry, `resume-website/src/_components/typography/EducationEntry.tsx`
    - [x] Changed the color temporarily, `resume-website/src/_components/layout/Sidebar.tsx`
    - [x] Modified general layout, `resume-website/src/app/page.tsx`

---
+++ resume-website/src/_components/typography/EducationEntry.tsx
@@ -0,0 +1,32 @@
+﻿import React from 'react';
+import Typography from "@mui/material/Typography";
+import {Box} from "@mui/material";
+
+interface IEducationEntryProps {
+    degree: string;
+    yearMonthCompletion: string;
+    school: string;
+    overallGpa: string;
+}
+
+const EducationEntry = ({
+                            degree,
+                            yearMonthCompletion,
+                            school,
+                            overallGpa
+                        }: IEducationEntryProps) => {
+    return (
+        <Box sx={{marginLeft: 2}}>
+            <Typography variant="subtitle1">{degree}</Typography>
+            <Typography variant="body2" color="text.secondary">{yearMonthCompletion}</Typography>
+            <Typography variant="body2" sx={{mt: 1}}>
+                {school}
+            </Typography>
+            <Typography variant="body2" sx={{mt: 1}}>
+                {overallGpa}
+            </Typography>
+        </Box>
+    );
+};
+
+export default EducationEntry;
