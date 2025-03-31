648151d9ad4f12d9e6f3061d4315ceeda214c5fe
Andrew Doser <Andrew.Doser@pkisolutions.com>
2025-03-30 17:00:19 -0700

    addressing sizing issues

--- resume-website/src/app/page.tsx
+++ resume-website/src/app/page.tsx
@@ -1,23 +1,18 @@
 import {Box, Grid } from "@mui/material";
 import React from "react";
 import ProfileCard from "@/_components/cards/ProfileCard";
+import EducationCard from "@/_components/cards/EducationCard";
 
 export default function Home() {
   return (
       <Box sx={{ display: "flex", justifyContent: "center" }}>
         <Box sx={{ flexGrow: 1 }} maxWidth={"lg"}>
           <Grid container direction="row" spacing={2} sx={{justifyContent: "center"}}>
-            <Grid size={4}>
+            <Grid size={{xs: 12, md: 4}} >
               <ProfileCard />
             </Grid>
-            <Grid size={4}>
-              test
-            </Grid>
-            <Grid size={4}>
-              test
-            </Grid>
-            <Grid size={8}>
-              test
+            <Grid size={{xs: 12, md: 8}}>
+              <EducationCard />
             </Grid>
           </Grid>
         </Box>
